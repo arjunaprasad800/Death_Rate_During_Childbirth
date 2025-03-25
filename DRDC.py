b7d0ff4dd1c7a3834b3613838a6a05c09846f9b1
Arjuna Prasad <125173478+arjunaprasad800@users.noreply.github.com>
2025-03-25 08:08:59 -0700

    Add files via upload

---
+++ DRDC.py
@@ -0,0 +1,38 @@
+import os
+import sys
+import tts from TTS
+
+chatbot = tts.Speech("What is your name?")
+tts.Speech()
+Patient= input("Data Entry For Patients")
+tts.Speech()
+Patient = input("Names and Date")
+tts.Speech()
+# as much as info we want
+def childbirth(Patientsage:int,childbirthno:int,childage:int,Patients_name:str)->int:
+    Patients_name= #input
+    childage = #input
+    childbirthno= #input
+    idealbreak= #input
+    average = childage*Patientsage/childno
+    birthratereduction= idealbreak-average
+    print(Patients_name,birthratereduction)
+    tts.Speech()
+def childdeath(childdeathno:int,childage:int,Patientsage:int,Patientsname:str)->int:
+    Patients_name= #input
+    childage = #input
+    childdeathno= #input
+    idealbreak= #input
+    average = childage*Patientsage/childno
+    deathratereduction= idealbreak+average
+    print(Patients_name,deathratereduction)
+    tts.Speech()
+
+def percentage(deathratereduction:int, birthratereduction:int)->int:
+    Patient= #input
+    potencyofthepatient = (birthratereduction-deathratereduction)/100
+    impotencyofthepatient(deathratereduction-birthratereduction)/100
+    print(f'The potency of the {patient} is {potencyofthepatient},The impotency of the {patient} is {impotencyofthepatient}')
+    tts.Speech()
+
+
